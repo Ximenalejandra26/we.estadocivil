0446b77af59a2b9317cf3bd2a8a3e840f57988a1
Ximena <Ximena.aleja021@outlook.com>
2024-11-04 19:33:16 -0500

    Utilizamos la declaración switch para convertir el número ingresado (1-5) en un texto descriptivo del estado civil

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 let nombre = prompt("Introduce tu nombre:");
 let estadoCivil = parseInt(prompt("Introduce tu estado civil:\n1: Soltero\n2: Casado\n3: Separado\n4: Viudo\n5: Unión libre"));
+
+let estadoCivilTexto;
+switch (estadoCivil) {
+    case 1:
+        estadoCivilTexto = "Soltero";
+        break;
+    case 2:
+        estadoCivilTexto = "Casado";
+        break;
+    case 3:
+        estadoCivilTexto = "Separado";
+        break;
+    case 4:
+        estadoCivilTexto = "Viudo";
+        break;
+    case 5:
+        estadoCivilTexto = "Unión libre";
+        break;
+    default:
+        document.write("Estado civil desconocido")
+}
