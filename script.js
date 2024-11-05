7c3aea0770b6b5e0e041098e2c3513027196ec60
ximena <ximena.aleja021@outlook.com>
2024-11-04 20:32:59 -0500

    let numero;: Definimos una variable numero que almacenará el valor que el usuario ingrese

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-let nombre = prompt("Introduce tu nombre:");
-let estadoCivil = parseInt(prompt("Introduce tu estado civil:\n1: Soltero\n2: Casado\n3: Separado\n4: Viudo\n5: Unión libre"));
+        let nombre = prompt('Introduce tu nombre');
+        let estadocivil =parseInt(prompt('Introduce tu estado civil,\n1 Soltero \n2 Casado \n3 Separado \n4 Viudo \n5 Unión libre'));
 
-let estadoCivilTexto;
-switch (estadoCivil) {
-    case 1:
-        estadoCivilTexto = "Soltero";
-        break;
-    case 2:
-        estadoCivilTexto = "Casado";
-        break;
-    case 3:
-        estadoCivilTexto = "Separado";
-        break;
-    case 4:
-        estadoCivilTexto = "Viudo";
-        break;
-    case 5:
-        estadoCivilTexto = "Unión libre";
-        break;
-    default:
-        document.write("Estado civil desconocido")
-}
+        switch(estadocivil){
+            case 1:
+                document.write('Soltero');
+                break;
+            case 2:
+                document.write('casado');
+                break;
+            case 3:
+                document.write('Separado');
+                break;
+            case 4:
+                document.write('viudo');
+                break;
+            case 5:
+                document.write('union libre');
+                break;
+            default:
+                document.write('Ingresa un valor de 1 a 5');
+             }
